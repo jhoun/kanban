50a13f6d7d1f2742e55e11970fbb84c31e2f85c0
jhoun <jayhoun@gmail.com>
2019-02-04 12:00:23 -1000

    Update developer docker

--- server/db/knex/knex.js
+++ server/db/knex/knex.js
@@ -1,4 +1,3 @@
 const environment = process.env.ENVIRONMENT || 'development'
-console.log('environmentKNEX', environment);
 const config = require('../../knexfile.js')[environment];
 module.exports = require('knex')(config);
